a971c652f32e6ab4d46c173fc9b2440e539e0bb6
birkin <birkin.diana@gmail.com>
2015-02-13 07:38:12 -0800

    cleans up temp Widget model admin.

--- dashboard_app/admin.py
+++ dashboard_app/admin.py
@@ -8,11 +8,11 @@ class WidgetAdmin( admin.ModelAdmin ):
     prepopulated_fields = {"slug": ("title",)}
     # date_hierarchy = u'create_datetime'
     ordering = [ u'id' ]
-    list_display = [ u'id', u'title', u'data_contact_email_address'  ]
+    list_display = [ u'id', u'title', u'data_contact_email_address' ]
     # list_filter = [ u'title' ]
-    search_fields = [ u'id', u'title', u'data_contact_email_address'  ]
-    readonly_fields = [ u'id', u'data_contact_email_address'  ]
-    radio_fields = {"best_goal": admin.HORIZONTAL}
+    search_fields = [ u'id', u'title', u'data_contact_email_address' ]
+    readonly_fields = [ u'id', u'baseline_value', u'best_value', u'data_contact_email_address', u'current_value', u'trend_color', u'trend_direction' ]
+    radio_fields = { u'best_goal': admin.HORIZONTAL, u'trend_color': admin.HORIZONTAL, u'trend_direction': admin.HORIZONTAL }
 
 
 admin.site.register( Widget, WidgetAdmin )
