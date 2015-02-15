4c415dc34258522ad91ab77f802133eb492d5edb
birkin <birkin.diana@gmail.com>
2015-02-15 06:45:57 -0800

    refactors test.

--- dashboard_app/tests.py
+++ dashboard_app/tests.py
@@ -10,51 +10,25 @@ class WidgetHelperTest(TestCase):
     """ Tests for models.WidgetHelper() """
 
     def test_process_data(self):
-        """ Tests function that processes Widget() instance data. """
-        ## init
+        """ Tests process_data(). """
         w = Widget()
-        w.best_goal = 1
+        w.best_goal = 1  # best is higher -- think higher circulation being good
         w.data_points = "[ ('97/98', 183179), ('98/99', 178095), ('99/00', 172425), ('00/01', 159397), ('01/02', 168697), ('02/03', 191740), ('03/04', 188981), ('04/05', 188298), ('05/06', 198735), ('06/07', 183533) ]"
         processed_w = widget_helper.processData( w )
         ## baseline
-        self.assertEqual(
-            183179, processed_w.baseline_value )
-
-
-        # expected = 183179
-        # result = processed_w.baseline_value
-        # self.assertTrue( expected == result, '\n Expected: ->%s<-; \nresult is: ->%s<-' % (expected, result,) )
-
-        # best
-        expected = 198735
-        result = processed_w.best_value
-        self.assertTrue( expected == result, '\n Expected: ->%s<-; \nresult is: ->%s<-' % (expected, result,) )
-
-        # current
-        expected = 183533
-        result = processed_w.current_value
-        self.assertTrue( expected == result, '\n Expected: ->%s<-; \nresult is: ->%s<-' % (expected, result,) )
-
-        # trend value
-        expected = -1
-        result = processed_w.trend_direction
-        self.assertTrue( expected == result, '\n Expected: ->%s<-; \nresult is: ->%s<-' % (expected, result,) )
-
-        # trend color
-        expected =  -1
-        result = processed_w.trend_color
-        self.assertTrue( expected == result, '\n Expected: ->%s<-; \nresult is: ->%s<-' % (expected, result,) )
-
-        ## handling bug in 'best' when best is 'lower'...
-
+        self.assertEqual( 183179, processed_w.baseline_value )
+        ## best
+        self.assertEqual( 198735, processed_w.best_value )
+        ## current
+        self.assertEqual( 183533, processed_w.current_value )
+        ## trend direction
+        self.assertEqual( -1, processed_w.trend_direction )
+        ## trend color
+        self.assertEqual( -1, processed_w.trend_color )
+        ## tests 'best' when best is 'lower', think missing books
         w2 = Widget()
         w2.best_goal = -1
         w2.data_points = "[ ('97/98', 183179), ('98/99', 178095), ('99/00', 172425), ('00/01', 159397), ('01/02', 168697), ('02/03', 191740), ('03/04', 188981), ('04/05', 188298), ('05/06', 198735), ('06/07', 183533) ]"
         processed_w2 = widget_helper.processData( w2 )
-
-        # best
-        expected = 159397
-        result = processed_w2.best_value
-        self.assertTrue( expected == result, '\n Expected: ->%s<-; \nresult is: ->%s<-' % (expected, result,) )
-
-        # end def test_processData()
+        ## best
+        self.assertEqual( 159397, processed_w2.best_value )
