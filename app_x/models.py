1ef6a690d8d74cb4ef86d6e118fc89d6308f8b22
birkin <birkin.diana@gmail.com>
2015-08-26 15:48:07 -0400

    adds unicode_literals.

--- app_x/models.py
+++ app_x/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import unicode_literals
 import datetime, json, logging, os, pprint, itertools
 from django.conf import settings as project_settings
 from django.core.urlresolvers import reverse
