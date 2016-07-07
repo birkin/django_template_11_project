7fa39d963d381df9420380e0324339c58331c280
birkin <birkin.diana@gmail.com>
2016-07-07 13:17:32 -0400

    adds default test.

--- app_x/tests.py
+++ app_x/tests.py
@@ -7,3 +7,23 @@ from django.test import TestCase
 
 log = logging.getLogger(__name__)
 TestCase.maxDiff = None
+
+
+class UrlTest( TestCase ):
+    """ Checks basic urls. """
+
+    def test_root_url_no_slash(self):
+        """ Checks '/root_url'. """
+        response = self.client.get( '' )  # project root part of url is assumed
+        self.assertEqual( 302, response.status_code )  # permanent redirect
+        redirect_url = response._headers['location'][1]
+        self.assertEqual(  '/info/', redirect_url )
+
+    def test_root_url_slash(self):
+        """ Checks '/root_url/'. """
+        response = self.client.get( '/' )  # project root part of url is assumed
+        self.assertEqual( 302, response.status_code )  # permanent redirect
+        redirect_url = response._headers['location'][1]
+        self.assertEqual(  '/info/', redirect_url )
+
+    # end class UrlTest()
