7943e429e6e9a70d80acedfcb50e8ce1ea2826f3
birkin <birkin.diana@gmail.com>
2016-11-09 13:14:09 -0500

    updates urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import patterns, include, url
+from __future__ import unicode_literals
+from django.conf.urls import include, url
 from django.contrib import admin
 
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
 
     url( r'^admin/', include(admin.site.urls) ),  # eg host/project_x/admin/
 
     url( r'^', include('app_x.urls_app') ),  # eg host/project_x/anything/
 
-)
+    ]
