63a7eef3c9cd044e841ae0c32bd263d93cbaa0af
birkin <birkin.diana@gmail.com>
2017-04-21 12:24:52 -0400

    updates to simplified wsgi-config.

--- config/passenger_wsgi.py
+++ config/passenger_wsgi.py
@@ -1,54 +1,43 @@
 # -*- coding: utf-8 -*-
 
 """
-WSGI config for django_template_project.
+WSGI config.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 """
-Prepares application environment.
-Variables assume project setup like:
-stuff
-    django_template_project
-        app_x
-        config
-    env_min_djng
+Note: no need to activate the virtual-environment
+- the project's httpd/passenger.conf section allows specification of the python-path via `PassengerPython`, which auto-activates it.
+- the auto-activation provides access to modules, but not, automatically, env-vars.
+- env-vars loading under python3.x occurs via the `SenEnv` entry in the project's passenger.conf section.
+  - `SenEnv` requires apache env_module; info: <https://www.phusionpassenger.com/library/indepth/environment_variables.html>
 """
 
 import os, pprint, sys
+import shellvars
 
 
-## become self-aware, padawan
-current_directory = os.path.dirname(os.path.abspath(__file__))
+# print( 'the initial env, ```{}```'.format( pprint.pformat(dict(os.environ)) ) )
 
-## vars
-ACTIVATE_FILE = os.path.abspath( u'%s/../../env_min_djng/bin/activate_this.py' % current_directory )
-PROJECT_DIR = os.path.abspath( u'%s/../../django_template_project' % current_directory )
-PROJECT_ENCLOSING_DIR = os.path.abspath( u'%s/../..' % current_directory )
-SETTINGS_MODULE = u'config.settings'
-SITE_PACKAGES_DIR = os.path.abspath( u'%s/../../env_min_djng/lib/python2.7/site-packages' % current_directory )
+PROJECT_DIR_PATH = os.path.dirname( os.path.dirname(os.path.abspath(__file__)) )
+ENV_SETTINGS_FILE = os.environ['DJANGO_TEMPLATE__SETTINGS_PATH']  # set in `conf.d/passenger.conf`, and `env/bin/activate`
 
-## virtualenv
-execfile( ACTIVATE_FILE, dict(__file__=ACTIVATE_FILE) )
+## update path
+sys.path.append( PROJECT_DIR_PATH )
 
-## sys.path additions
-for entry in [PROJECT_DIR, PROJECT_ENCLOSING_DIR, SITE_PACKAGES_DIR]:
- if entry not in sys.path:
-   sys.path.append( entry )
-
-## environment additions
-os.environ[u'DJANGO_SETTINGS_MODULE'] = SETTINGS_MODULE  # so django can access its settings
+## reference django settings
+os.environ[u'DJANGO_SETTINGS_MODULE'] = 'config.settings'  # so django can access its settings
 
 ## load up env vars
-SETTINGS_FILE = os.environ['DJANGO_TEMPLATE__SETTINGS_PATH']  # set in activate_this.py, and activated above
-import shellvars
-var_dct = shellvars.get_vars( SETTINGS_FILE )
+var_dct = shellvars.get_vars( ENV_SETTINGS_FILE )
 for ( key, val ) in var_dct.items():
-    os.environ[key] = val
+    os.environ[key.decode('utf-8')] = val.decode('utf-8')
+
+# print( 'the final env, ```{}```'.format( pprint.pformat(dict(os.environ)) ) )
 
 ## gogogo
 from django.core.wsgi import get_wsgi_application
