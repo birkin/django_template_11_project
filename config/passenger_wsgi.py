f34b000a867951a466f1a62b0842d87c527d823d
birkin <birkin.diana@gmail.com>
2017-04-21 12:55:02 -0400

    updates comments.

--- config/passenger_wsgi.py
+++ config/passenger_wsgi.py
@@ -10,11 +10,18 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 """
-Note: no need to activate the virtual-environment
+Note: no need to activate the virtual-environment here for passenger.
 - the project's httpd/passenger.conf section allows specification of the python-path via `PassengerPython`, which auto-activates it.
 - the auto-activation provides access to modules, but not, automatically, env-vars.
-- env-vars loading under python3.x occurs via the `SenEnv` entry in the project's passenger.conf section.
-  - `SenEnv` requires apache env_module; info: <https://www.phusionpassenger.com/library/indepth/environment_variables.html>
+- passenger env-vars loading under python3.x is enabled via the `SenEnv` entry in the project's httpd/passenger.conf section.
+  - usage: `SetEnv DJANGO_TEMPLATE__SETTINGS_PATH /path/to/django_template__env_settings.sh`
+  - `SenEnv` requires apache env_module; info: <https://www.phusionpassenger.com/library/indepth/environment_variables.html>,
+     enabled by default on macOS 10.12.4, and our dev and production servers.
+
+For activating the virtual-environment manually, don't source the settings file directly. Instead, add to `project_env/bin/activate`:
+  export DJANGO_TEMPLATE__SETTINGS_PATH="/path/to/django_template__env_settings.sh"
+  source $DJANGO_TEMPLATE__SETTINGS_PATH
+This allows not only the sourcing, but also creates the env-var used below by shellvars.
 """
 
 import os, pprint, sys
@@ -24,7 +31,7 @@ import shellvars
 # print( 'the initial env, ```{}```'.format( pprint.pformat(dict(os.environ)) ) )
 
 PROJECT_DIR_PATH = os.path.dirname( os.path.dirname(os.path.abspath(__file__)) )
-ENV_SETTINGS_FILE = os.environ['DJANGO_TEMPLATE__SETTINGS_PATH']  # set in `conf.d/passenger.conf`, and `env/bin/activate`
+ENV_SETTINGS_FILE = os.environ['DJANGO_TEMPLATE__SETTINGS_PATH']  # set in `httpd/passenger.conf`, and `env/bin/activate`
 
 ## update path
 sys.path.append( PROJECT_DIR_PATH )
