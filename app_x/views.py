b336e779ec06ecca5a0d4cf558f92d17db50ccf1
birkin <birkin.diana@gmail.com>
2017-12-19 10:33:42 -0500

    adds comments.

--- app_x/views.py
+++ app_x/views.py
@@ -11,13 +11,14 @@ log = logging.getLogger(__name__)
 
 
 def info( request ):
-    """ Returns basic info. """
+    """ Returns basic info.
+        Getting this running shows that logging is working, and that the settings_app file is properly reading env-vars. """
     log.debug( 'request.__dict__, ```%s```' % pprint.pformat(request.__dict__) )
     start = datetime.datetime.now()
     rtrn_dct = {
         'query': {
             'date_time': str( start ),
-            'url': '{schm}://{hst}{uri}'.format( schm=request.scheme, hst=request.META['HTTP_HOST'], uri=request.META.get('REQUEST_URI', request.META['PATH_INFO']) ) },
+            'url': '{schm}://{hst}{uri}'.format( schm=request.scheme, hst=request.META['HTTP_HOST'], uri=request.META.get('REQUEST_URI', request.META['PATH_INFO']) ) },  # REQUEST_URI not available via run-server
         'response': {
             'documentation': settings_app.README_URL,
             'elapsed_time': str( datetime.datetime.now() - start ),
