a167d11129833f2628634c85a10790178f4125b4
birkin <birkin.diana@gmail.com>
2018-09-24 15:23:23 -0400

    updates default version/info view.

--- app_x/views.py
+++ app_x/views.py
@@ -2,26 +2,27 @@
 
 import datetime, json, logging, os, pprint
 from . import settings_app
+from app_x.lib import view_info_helper
 from django.conf import settings as project_settings
 from django.contrib.auth import logout
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 
+
 log = logging.getLogger(__name__)
 
 
 def info( request ):
-    """ Returns basic info.
-        Getting this running shows that logging is working, and that the settings_app file is properly reading env-vars. """
-    log.debug( 'request.__dict__, ```%s```' % pprint.pformat(request.__dict__) )
-    start = datetime.datetime.now()
-    rtrn_dct = {
-        'query': {
-            'date_time': str( start ),
-            'url': '{schm}://{hst}{uri}'.format( schm=request.scheme, hst=request.META['HTTP_HOST'], uri=request.META.get('REQUEST_URI', request.META['PATH_INFO']) ) },  # REQUEST_URI not available via run-server
-        'response': {
-            'documentation': settings_app.README_URL,
-            'elapsed_time': str( datetime.datetime.now() - start ),
-            'message': 'ok' } }
-    return HttpResponse( json.dumps(rtrn_dct, sort_keys=True, indent=2), content_type='application/javascript; charset=utf-8' )
+    """ Returns basic data including branch & commit. """
+    1/0  # tests admin error-emails (remove)
+    # log.debug( 'request.__dict__, ```%s```' % pprint.pformat(request.__dict__) )
+    rq_now = datetime.datetime.now()
+    commit = view_info_helper.get_commit()
+    branch = view_info_helper.get_branch()
+    info_txt = commit.replace( 'commit', branch )
+    resp_now = datetime.datetime.now()
+    taken = resp_now - rq_now
+    context_dct = view_info_helper.make_context( request, rq_now, info_txt, taken )
+    output = json.dumps( context_dct, sort_keys=True, indent=2 )
+    return HttpResponse( output, content_type='application/json; charset=utf-8' )
