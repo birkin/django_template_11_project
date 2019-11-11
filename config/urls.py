82c6d788771145eb8ca8c55e74d7c40825cffcb8
birkin <birkin.diana@gmail.com>
2019-11-11 07:20:40 -0500

    organizes urls.

--- config/urls.py
+++ config/urls.py
@@ -11,15 +11,14 @@ admin.autodiscover()
 
 urlpatterns = [
 
-    # url( r'^admin/login/', RedirectView.as_view(pattern_name='login_url') ),
-
+    ## primary app urls...
     url( r'^admin/', admin.site.urls ),  # eg host/project_x/admin/
-
-    url( r'^version/$', views.version, name='version_url' ),
-
-    url( r'^error_check/$', views.error_check, name='error_check_url' ),
-
+    # url( r'^admin/login/', RedirectView.as_view(pattern_name='login_url') ),
     # url( r'^login/$', views.login, name='login_url' ),
+
+    ## support urls...
+    url( r'^version/$', views.version, name='version_url' ),
+    url( r'^error_check/$', views.error_check, name='error_check_url' ),
 
     url( r'^$', RedirectView.as_view(pattern_name='version_url') ),
 
