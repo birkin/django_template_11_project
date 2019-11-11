d71dfaf0c5838931e0d0136bc06e025fad4c2984
birkin <birkin.diana@gmail.com>
2019-11-11 07:23:00 -0500

    organizes view-handlers.

--- app_x/views.py
+++ app_x/views.py
@@ -14,6 +14,16 @@ from django.shortcuts import get_object_or_404, render
 log = logging.getLogger(__name__)
 
 
+# def dummy_url_handler( request ):
+#     """ Docstring. """
+#     pass
+
+
+# ===========================
+# for development convenience
+# ===========================
+
+
 def version( request ):
     """ Returns basic data including branch & commit. """
     # log.debug( 'request.__dict__, ```%s```' % pprint.pformat(request.__dict__) )
