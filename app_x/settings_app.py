a1e7d066dabebeff64b70f7903561d2c59d52bc1
birkin <birkin.diana@gmail.com>
2019-11-12 11:56:39 -0500

    updates envar reference.

--- app_x/settings_app.py
+++ app_x/settings_app.py
@@ -6,7 +6,7 @@ import json, os
 README_URL = os.environ['DJANGO_TEMPLATE__README_URL']
 
 ## auth
-# SUPER_USERS = json.loads( os.environ['DJANGO_TEMPLATE__UPER_USERS_JSON'] )
+# SUPER_USERS = json.loads( os.environ['DJANGO_TEMPLATE__SUPER_USERS_JSON'] )
 # STAFF_USERS = json.loads( os.environ['DJANGO_TEMPLATE__STAFF_USERS_JSON'] )  # users permitted access to admin
 # STAFF_GROUP = os.environ['DJANGO_TEMPLATE__STAFF_GROUP']  # not grouper-group; rather, name of django-admin group for permissions
 # TEST_META_DCT = json.loads( os.environ['DJANGO_TEMPLATE__TEST_META_DCT_JSON'] )  # for local-developemnt simulating shib-data
